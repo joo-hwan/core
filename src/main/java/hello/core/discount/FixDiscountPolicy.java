61b013ce5392af9667b6d81a598da29b59afd5d0
211947 <joohwan@ssg.com>
2022-12-18 23:23:41 +0900

    의존관계 자동주입

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,11 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 
+//@MainDiscountPolicy
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
